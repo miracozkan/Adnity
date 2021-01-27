5b9de61932d7f2db7410d4f5283403d8dc411e2a
Miraç Özkan <mirac.ozkan123@gmail.com>
2021-01-27 23:19:52 +0300

    Add Adnity as dependency

--- app/src/main/java/com/mihanitylabs/adnity/MainActivity.kt
+++ app/src/main/java/com/mihanitylabs/adnity/MainActivity.kt
@@ -2,10 +2,62 @@ package com.mihanitylabs.adnity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.mihanitylabs.adnitylib.bannerad.BannerAd
+import com.mihanitylabs.adnitylib.bannerad.BannerAdConfig
+import com.mihanitylabs.adnitylib.interstitialad.InterstitialAd
+import com.mihanitylabs.adnitylib.interstitialad.InterstitialAdConfig
+import com.mihanitylabs.adnitylib.interstitialad.InterstitialAdScheduler
+import com.mihanitylabs.adnitylib.rewardedad.RewardedAd
+import com.mihanitylabs.adnitylib.rewardedad.RewardedAdConfig
 
 class MainActivity : AppCompatActivity() {
+
+    private val interstitialAdConfig = InterstitialAdConfig(
+        adId = INTERSTITIAL_AD_ID,
+        onAdShowedFullScreen = {},
+        onDismissed = {},
+        onFailed = {},
+        onLoading = {}
+    )
+
+    private val rewardedAdConfig = RewardedAdConfig(
+        adId = REWARDED_AD_ID,
+        onAdShowedFullScreen = {},
+        onFailed = {},
+        onLoading = {}
+    )
+
+    private val bannerAdConfig = BannerAdConfig(
+        adId = BANNER_AD_ID,
+        adSize = AdSize.BANNER,
+        onAdClicked = {},
+        onAdFailedToLoad = {},
+        onAdLoaded = {},
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RewardedAd.displayRewardedAd(this, rewardedAdConfig)
+
+        InterstitialAd.displayInterstitial(this, interstitialAdConfig)
+
+        val adView: AdView = BannerAd.provideBannerAd(this, bannerAdConfig)
+
+        val interstitialAdScheduler = InterstitialAdScheduler.getInstance().apply {
+            init(5)
+            setOnTimeFinishedListener {
+                InterstitialAd.displayInterstitial(this@MainActivity, interstitialAdConfig)
+            }
+        }
+    }
+
+    companion object {
+        private const val INTERSTITIAL_AD_ID = ""
+        private const val BANNER_AD_ID = ""
+        private const val REWARDED_AD_ID = ""
     }
 }
--- app/src/main/java/com/mihanitylabs/adnity/MyApplication.kt
+++ app/src/main/java/com/mihanitylabs/adnity/MyApplication.kt
@@ -1,6 +1,9 @@
 package com.mihanitylabs.adnity
 
 import android.app.Application
+import com.mihanitylabs.adnitylib.openappad.AppOpenAdConfig
+import com.mihanitylabs.adnitylib.openappad.AppOpenAdManager
+import com.mihanitylabs.adnitylib.util.initAMobileAds
 
 
 // Code with ❤️
@@ -12,4 +15,23 @@ import android.app.Application
 //│ 1/26/2021 - 9:40 PM         │
 //└─────────────────────────────┘
 
-class MyApplication : Application()
+class MyApplication : Application() {
+
+    private lateinit var appOpenAdManager: AppOpenAdManager
+
+    private val appOpenAdConfig = AppOpenAdConfig(
+        openAppAdId = APP_OPEN_AD_ID,
+        onSuccess = {},
+        onError = {}
+    )
+
+    override fun onCreate() {
+        super.onCreate()
+        initAMobileAds()
+        appOpenAdManager = AppOpenAdManager(this, appOpenAdConfig)
+    }
+
+    companion object {
+        private const val APP_OPEN_AD_ID = ""
+    }
+}
